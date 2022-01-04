afbc30b1737b6701913be2f59362900cd9bd21a9
hrjy6278 <hrjy6278@naver.com>
2022-01-04 16:19:01 +0900

    챕터8 완료

--- 08-transforming-operators-in-practice/projects/starter/GitFeed/ActivityController.swift
+++ 08-transforming-operators-in-practice/projects/starter/GitFeed/ActivityController.swift
@@ -35,7 +35,20 @@ import RxSwift
 import RxCocoa
 import Kingfisher
 
+func cacheFileURL(_ fileName: String) -> URL {
+  return FileManager.default.urls(for: .cachesDirectory, in: .allDomainsMask)
+    .first!
+    .appendingPathComponent(fileName)
+}
+
 class ActivityController: UITableViewController {
+  //이벤트 URL을 disk에 저장하기위한 프로퍼티이다.
+  private let eventsFileURL = cacheFileURL("events.json")
+  
+  //새로운 데이터만 가져올 수 있도록 이전 네트워크 응답 헤더의 수신날짜를 저장하는 프로퍼티
+  private let modifiedFileURL = cacheFileURL("modified.txt")
+  private let lastModified = BehaviorRelay<String?>(value: nil)
+  
   private let repo = "ReactiveX/RxSwift"
 
   private let events = BehaviorRelay<[Event]>(value: [])
@@ -52,8 +65,20 @@ class ActivityController: UITableViewController {
     refreshControl.tintColor = UIColor.darkGray
     refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
     refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
-
-    refresh()
+    
+    
+    if let lastModifiedString = try? String(contentsOf: modifiedFileURL, encoding: .utf8) {
+      lastModified.accept(lastModifiedString)
+    }
+    
+    //로컬에 캐쉬 데이터가 있으면 해당 데이터를 불러오고, 아니면 새롭게 네트워크 요청
+    if let eventsData = try? Data(contentsOf: eventsFileURL),
+       let persistedEvents = try? JSONDecoder().decode([Event].self, from: eventsData) {
+      events.accept(persistedEvents)
+    } else {
+      refresh()
+    }
+    
   }
 
   @objc func refresh() {
@@ -64,11 +89,81 @@ class ActivityController: UITableViewController {
   }
 
   func fetchEvents(repo: String) {
+    //옵저버블을 만든다. from으로  맵을 사용하여 URL -> URLRequest 로 변환시킴.
+    let response = Observable.from([repo])
+      .subscribeOn(CurrentThreadScheduler.instance)
+      .map { urlString in
+        return URL(string: "https://api.github.com/repos/\(urlString)/events")!
+      }
+    //기존에는 .map { URLRequest(url: $0) } 이였으나 헤더에 마지막 다운로드 시간을 넣는 로직 추가
+      .map { [weak self] in
+        var request = URLRequest(url: $0)
+        if let modifiedHeader = self?.lastModified.value {
+          request.addValue(modifiedHeader,
+                           forHTTPHeaderField: "Last-Modified")
+        }
+        return request
+      }
+    // flatMap을 활용하여 새로운 옵저버블을 만든다.
+    // 그리고 URLSession.rx를 활용하여 옵저버블을 만든다.
+      .flatMap { request -> Observable<(response: HTTPURLResponse, data: Data)> in
+        return URLSession.shared.rx.response(request: request)
+    // 이전에 네트워크 응답결과를 캐쉬로 가지고 있는다. 구독자가 새로 생기면 이전에 요청한 네트워크 응답을 리턴한다.
+      }.share(replay: 1)
+    
+    //httpResponse Code가 200~300 성공 응답일시에 subscibe 하는 부분
+    response
+      .filter { response, _ in
+      return 200..<300 ~= response.statusCode
+    }
+      .compactMap { _, data -> [Event]? in
+      return try? JSONDecoder().decode([Event].self, from: data)
+    }
+      .observeOn(MainScheduler.instance)
+      .subscribe(onNext: { [weak self] newEvents in
+        self?.processEvents(newEvents)
+      })
+      .disposed(by: bag)
 
+    //response header의 마지막 다운로드 시간을 저장하는 부분
+    response
+      .filter { response, _ in
+        return 200..<400 ~= response.statusCode
+      }
+      .flatMap { response, _ -> Observable<String> in
+        guard let headerValue = response.allHeaderFields["Last-Modified"] as? String else {
+          return Observable.never()
+        }
+        return Observable.just(headerValue)
+      }
+      .subscribe(onNext: { [weak self] modifiedHeader in
+        guard let self = self else { return }
+        self.lastModified.accept(modifiedHeader)
+        try? modifiedHeader.write(to: self.modifiedFileURL,
+                                  atomically: true,
+                                  encoding: .utf8)
+      })
+      .disposed(by: bag)
   }
   
   func processEvents(_ newEvents: [Event]) {
+    //깃허브 네트워크 통신을 하고 난 뒤 Data를 View에 띄우는 작업
+    var updatedEvents = newEvents + events.value
+    if updatedEvents.count > 50 {
+      updatedEvents = [Event](updatedEvents.prefix(upTo: 50))
+    }
     
+    events.accept(updatedEvents)
+    tableView.reloadData()
+    self.refreshControl?.endRefreshing()
+    
+    //eventsData를 내부 디스크에 저장한다.
+    DispatchQueue.global().async {
+      let encoder = JSONEncoder()
+      if let eventsData = try? encoder.encode(updatedEvents) {
+        try? eventsData.write(to: self.eventsFileURL, options: .atomic)
+      }
+    }
   }
 
   // MARK: - Table Data Source
