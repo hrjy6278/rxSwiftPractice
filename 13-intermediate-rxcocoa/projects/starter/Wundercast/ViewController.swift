9a5f68b32c4fb61c26239306a27c9ddd3530a584
hrjy6278 <hrjy6278@naver.com>
2022-01-10 16:56:40 +0900

    챕터 13 RxSwift + CoreLocation을 활용한 현재위치기반 날씨정보 가져오기 구현

--- 13-intermediate-rxcocoa/projects/starter/Wundercast/ViewController.swift
+++ 13-intermediate-rxcocoa/projects/starter/Wundercast/ViewController.swift
@@ -34,82 +34,165 @@ import UIKit
 import RxSwift
 import RxCocoa
 import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-  @IBOutlet private var mapView: MKMapView!
-  @IBOutlet private var mapButton: UIButton!
-  @IBOutlet private var geoLocationButton: UIButton!
-  @IBOutlet private var activityIndicator: UIActivityIndicatorView!
-  @IBOutlet private var searchCityName: UITextField!
-  @IBOutlet private var tempLabel: UILabel!
-  @IBOutlet private var humidityLabel: UILabel!
-  @IBOutlet private var iconLabel: UILabel!
-  @IBOutlet private var cityNameLabel: UILabel!
-
-  private let bag = DisposeBag()
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    style()
-
-    let search = searchCityName.rx
-      .controlEvent(.editingDidEndOnExit)
-      .map { self.searchCityName.text ?? "" }
-      .filter { !$0.isEmpty }
-      .flatMapLatest { text in
-        ApiController.shared
-          .currentWeather(for: text)
-          .catchErrorJustReturn(.empty)
-      }
-      .asDriver(onErrorJustReturn: .empty)
-
-    search.map { "\($0.temperature)° C" }
-      .drive(tempLabel.rx.text)
-      .disposed(by: bag)
-
-    search.map(\.icon)
-      .drive(iconLabel.rx.text)
-      .disposed(by: bag)
-
-    search.map { "\($0.humidity)%" }
-      .drive(humidityLabel.rx.text)
-      .disposed(by: bag)
-
-    search.map(\.cityName)
-      .drive(cityNameLabel.rx.text)
-      .disposed(by: bag)
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-  }
-
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-
-    Appearance.applyBottomLine(to: searchCityName)
-  }
-
-  override var preferredStatusBarStyle: UIStatusBarStyle {
-    return .lightContent
-  }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  // MARK: - Style
-
-  private func style() {
-    view.backgroundColor = UIColor.aztec
-    searchCityName.attributedPlaceholder = NSAttributedString(string: "City's Name",
-                                                              attributes: [.foregroundColor: UIColor.textGrey])
-    searchCityName.textColor = UIColor.ufoGreen
-    tempLabel.textColor = UIColor.cream
-    humidityLabel.textColor = UIColor.cream
-    iconLabel.textColor = UIColor.cream
-    cityNameLabel.textColor = UIColor.cream
-  }
+    @IBOutlet private var mapView: MKMapView!
+    @IBOutlet private var mapButton: UIButton!
+    @IBOutlet private var geoLocationButton: UIButton!
+    @IBOutlet private var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet private var searchCityName: UITextField!
+    @IBOutlet private var tempLabel: UILabel!
+    @IBOutlet private var humidityLabel: UILabel!
+    @IBOutlet private var iconLabel: UILabel!
+    @IBOutlet private var cityNameLabel: UILabel!
+    
+    private let bag = DisposeBag()
+    private let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        
+        
+        //사용자의 위치 데이터를 얻기 위한 권한 얻기 로직
+        //리팩토링 진행
+        //        geoLocationButton.rx.tap
+        //            .subscribe(onNext: { [weak self] _ in
+        //                guard let self = self else { return }
+        //
+        //                self.locationManager.requestWhenInUseAuthorization()
+        //                self.locationManager.startUpdatingLocation()
+        //            })
+        //            .disposed(by: bag)
+        //
+        //        locationManager.rx.didUpdateLocations
+        //            .subscribe(onNext: { locations in
+        //                print(locations)
+        //            })
+        //            .disposed(by: bag)
+        
+        let geoSearch = geoLocationButton.rx.tap
+            .flatMapLatest { _ in
+                self.locationManager.rx.getCurrentLocation()
+            }
+            .flatMapLatest { location in
+                ApiController
+                    .shared
+                    .currentWeather(at: location.coordinate)
+                    .catchErrorJustReturn(.empty)
+            }
+        
+        let searchInput = searchCityName.rx
+            .controlEvent(.editingDidEndOnExit)
+            .map { self.searchCityName.text ?? "" }
+            .filter { !$0.isEmpty }
+        
+        
+       
+        let textSearch = searchInput.flatMap { city in
+            ApiController
+                .shared
+                .currentWeather(for: city)
+                .catchErrorJustReturn(.empty)
+        }
+        
+//        이전 코드 리팩토링 진행
+//        let search = searchInput
+//            .flatMapLatest { text in
+//                ApiController.shared
+//                    .currentWeather(for: text)
+//                    .catchErrorJustReturn(.empty)
+//            }
+//            .asDriver(onErrorJustReturn: .empty)
+        
+        let search = Observable.merge(geoSearch, textSearch)
+            .asDriver(onErrorJustReturn: .empty)
+        
+        
+        
+        //네트워크 통신중인지 아닌지 판단하는 옵저버블
+        //기존 코드.. 리팩토링 진행
+//        let running = Observable.merge(searchInput.map { _ in true },
+//                                       search.map { _ in false }.asObservable())
+//            .startWith(true)
+//            .asDriver(onErrorJustReturn: false)
+        
+        let running = Observable.merge(searchInput.map { _ in true },
+                                       search.map { _ in false }.asObservable(),
+                                       geoLocationButton.rx.tap.map { _ in true })
+            .startWith(true)
+            .asDriver(onErrorJustReturn: false)
+        
+        //네트워크 통신에 따른 레이블들을 숨기거나 보여주게 하는 로직
+        running
+            .skip(1)
+            .drive(activityIndicator.rx.isAnimating)
+            .disposed(by: bag)
+        
+        running
+            .drive(tempLabel.rx.isHidden)
+            .disposed(by: bag)
+        
+        running
+            .drive(iconLabel.rx.isHidden)
+            .disposed(by: bag)
+        
+        running
+            .drive(humidityLabel.rx.isHidden)
+            .disposed(by: bag)
+        
+        running
+            .drive(cityNameLabel.rx.isHidden)
+            .disposed(by: bag)
+        
+        
+        search.map { "\($0.temperature)° C" }
+        .drive(tempLabel.rx.text)
+        .disposed(by: bag)
+        
+        search.map(\.icon)
+            .drive(iconLabel.rx.text)
+            .disposed(by: bag)
+        
+        search.map { "\($0.humidity)%" }
+        .drive(humidityLabel.rx.text)
+        .disposed(by: bag)
+        
+        search.map(\.cityName)
+            .drive(cityNameLabel.rx.text)
+            .disposed(by: bag)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        Appearance.applyBottomLine(to: searchCityName)
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Style
+    
+    private func style() {
+        view.backgroundColor = UIColor.aztec
+        searchCityName.attributedPlaceholder = NSAttributedString(string: "City's Name",
+                                                                  attributes: [.foregroundColor: UIColor.textGrey])
+        searchCityName.textColor = UIColor.ufoGreen
+        tempLabel.textColor = UIColor.cream
+        humidityLabel.textColor = UIColor.cream
+        iconLabel.textColor = UIColor.cream
+        cityNameLabel.textColor = UIColor.cream
+    }
 }
