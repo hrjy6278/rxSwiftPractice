f2f7af422eed62bd755541bfc890e6d810f9a739
hrjy6278 <hrjy6278@naver.com>
2022-01-11 20:23:45 +0900

    챕터 16 진행중

--- 16-testing-with-rxtests/projects/starter/Testing/TestingTests/TestingOperators.swift
+++ 16-testing-with-rxtests/projects/starter/Testing/TestingTests/TestingOperators.swift
@@ -36,14 +36,94 @@ import RxTest
 import RxBlocking
 
 class TestingOperators : XCTestCase {
-  var scheduler: TestScheduler!
-  var subscription: Disposable!
-
-  override func setUp() {
-    super.setUp()
-  }
-
-  override func tearDown() {
-    super.tearDown()
-  }
+    var scheduler: TestScheduler!
+    var subscription: Disposable!
+    
+    override func setUp() {
+        super.setUp()
+        scheduler = TestScheduler(initialClock: 0)
+    }
+    
+    override func tearDown() {
+        scheduler.scheduleAt(1000) {
+            self.subscription.dispose()
+        }
+        
+        scheduler = nil
+        
+        super.tearDown()
+    }
+    
+    func testAmb() {
+        
+        let observer = scheduler.createObserver(String.self)
+        
+        //1
+        let observableA = scheduler.createHotObservable([
+            //2
+            .next(100, "a"),
+            .next(200, "b"),
+            .next(300, "c"),
+        ])
+        
+        //3
+        let observableB = scheduler.createHotObservable([
+            //4
+            .next(90, "1"),
+            .next(200, "2"),
+            .next(300, "3"),
+        ])
+        
+        let ambObservable = observableA.amb(observableB)
+        
+        self.subscription = ambObservable.subscribe(observer)
+        
+        scheduler.start()
+        
+        let results = observer.events.compactMap {
+            $0.value.element
+        }
+        
+        XCTAssertEqual(results, ["1","2","3"])
+    }
+    
+    func testFilter() {
+        let observer = scheduler.createObserver(Int.self)
+        
+        let observable = scheduler.createHotObservable([
+            .next(100, 1),
+            .next(200, 2),
+            .next(300, 3),
+            .next(400, 2),
+            .next(500, 1),
+        ])
+        
+        let filterObservable = observable.filter {
+            $0 < 3
+        }
+        
+        scheduler.scheduleAt(0) {
+            self.subscription = filterObservable.subscribe(observer)
+        }
+        
+        scheduler.start()
+        
+        let results = observer.events.compactMap {
+            $0.value.element
+        }
+        
+        XCTAssertEqual(results, [1,2,2,1])
+    }
+    
+    //RxBlocking 사용한 Unit Test 진행
+    func testToArray() throws {
+        //1
+        let scheduler = ConcurrentDispatchQueueScheduler(qos: .default)
+        
+        //2
+        let toArrayObservable = Observable.of(1,2).subscribeOn(scheduler)
+        
+        //3
+        XCTAssertEqual(try toArrayObservable.toBlocking().toArray(), [1,2])
+    }
 }
